7c88e19a2fd456cfce24cd4a8c62f7a7ce3e3d78
Kaeneth Dayao <kendayao@gmail.com>
2020-07-22 11:46:46 -0700

    changed linkedin link

--- client/src/components/Connect.js
+++ client/src/components/Connect.js
@@ -9,7 +9,7 @@ function Connect(){
             <div className="card-body">
               <h5 className="card-title">Connect with me</h5>
               <hr/>
-              <a href="https://www.linkedin.com/in/kaeneth-dayao-3031808b/" target="_blank" title="Linkedin Profile"><i className="fab fa-linkedin fa-5x"></i></a>
+              <a href="https://www.linkedin.com/in/kaeneth-dayao/" target="_blank" title="Linkedin Profile"><i className="fab fa-linkedin fa-5x"></i></a>
               <a href="https://github.com/kendayao" target="_blank" title="Github"><i className="fab fa-github-square fa-5x"></i></a>
               <a href="mailto:kendayao@gmail.com" title="Email Me"><i className="fas fa-at fa-5x"></i></a>
               <a href="../../resume/resume3.pdf" target="_blank" title="Resume"><i className="far fa-file-pdf fa-5x"></i></a>
