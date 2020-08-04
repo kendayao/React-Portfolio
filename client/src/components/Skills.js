d6470f46830ab1d24f5403282af259924dc0226f
Kaeneth Dayao <kendayao@gmail.com>
2020-08-04 12:30:04 -0700

    resume fix, padding added to icons

--- client/src/components/Skills.js
+++ client/src/components/Skills.js
@@ -9,19 +9,19 @@ return (
             <div className="card-body">
               <h5 className="card-title">My Skills</h5>
               <hr/>
-              <i className="fab fa-html5 fa-3x" title="HTML"></i>
-              <i className="fab fa-node fa-3x" title="Node.js"></i>
-              <i className="fab fa-css3-alt fa-3x" title="CSS"></i>
-              <img className="img1" title="MySQL" src="../images/mysql.png" alt="mysql" />
-              <i className="fab fa-bootstrap fa-3x" title="Bootstrap"></i>
-              <i className="fab fa-github fa-3x" title="Github"></i>
-              <i className="fab fa-react fa-3x" title="REACT" ></i>
-              <img className="img1" title="jQuery" src="../images/jquery.png" alt="jquery"/>
-              <img className="img1" title="MERN" src="../images/mern.png" alt="Mern"/>
-              <img className="img1" title="MongoDB" src="../images/mongdb.png" alt="mongodb"/>
-              <img className="img1" title="Sequelize" src="../images/sequelize.png" alt="sequelize"/>
-              <i className="fab fa-js fa-3x" title="JavaScript" ></i>
-              <i className="fab fa-python fa-3x" title="Python"></i>
+              <i className="fab fa-html5 fa-3x skills-icon" title="HTML"></i>
+              <i className="fab fa-node fa-3x skills-icon" title="Node.js"></i>
+              <i className="fab fa-css3-alt fa-3x skills-icon" title="CSS"></i>
+              <img className="img1 skills-icon" title="MySQL" src="../images/mysql.png" alt="mysql" />
+              <i className="fab fa-bootstrap fa-3x skills-icon" title="Bootstrap"></i>
+              <i className="fab fa-github fa-3x skills-icon" title="Github"></i>
+              <i className="fab fa-react fa-3x skills-icon" title="REACT" ></i>
+              <img className="img1 skills-icon" title="jQuery" src="../images/jquery.png" alt="jquery"/>
+              <img className="img1 skills-icon" title="MERN" src="../images/mern.png" alt="Mern"/>
+              <img className="img1 skills-icon" title="MongoDB" src="../images/mongdb.png" alt="mongodb"/>
+              <img className="img1 skills-icon" title="Sequelize" src="../images/sequelize.png" alt="sequelize"/>
+              <i className="fab fa-js fa-3x skills-icon" title="JavaScript" ></i>
+              <i className="fab fa-python fa-3x skills-icon" title="Python"></i>
             </div>
           </div>
         </div>
