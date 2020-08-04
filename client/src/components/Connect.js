4d4a03ce7f4dde8df11638abe67c968018266d83
Kaeneth Dayao <kendayao@gmail.com>
2020-08-03 20:23:54 -0700

    changed resume

--- client/src/components/Connect.js
+++ client/src/components/Connect.js
@@ -11,23 +11,23 @@ function Connect(){
               <hr/>
               <div className="row">
                 
-                  <div className="col-lg-4 col-md-6 icon">
+                  <div className="col-lg-4 col-md-6 col-sm-6 icon">
                   <a href="https://www.linkedin.com/in/kaeneth-dayao/" target="_blank" title="Linkedin Profile"><i className="fab fa-linkedin fa-4x"></i></a>
                   <p>Linkedin</p>
                   </div>
-                <div className="col-lg-4 col-md-6 icon">
+                <div className="col-lg-4 col-md-6 col-sm-6 icon">
                   <a href="https://github.com/kendayao" target="_blank" title="Github"><i className="fab fa-github-square fa-4x"></i></a>
                   <p>Github</p>
                 </div>
-                <div className="col-lg-4 col-md-6 icon">
+                <div className="col-lg-4 col-md-6 col-sm-6 icon">
                   <a href="mailto:kendayao@gmail.com" title="Email Me"><i className="fas fa-at fa-4x"></i></a>
                   <p>Email</p>
                 </div>
-                <div className="col-lg-4 col-md-6 icon">
+                <div className="col-lg-4 col-md-6 col-sm-6 icon">
                   <a href="../../resume/resume3.pdf" target="_blank" title="Resume"><i className="far fa-file-pdf fa-4x"></i></a>
                   <p>Resume</p>
                 </div>
-                <div className="col-lg-4 col-md-6 icon">
+                <div className="col-lg-4 col-md-6 col-sm-6 icon">
                   <a href="https://www.youtube.com/channel/UCcD-il-fawZbObChZPpRfrw" target="_blank" title="Youtube"><i class="fab fa-youtube fa-4x"></i></a>
                   <p>Youtube</p>
                 </div>
--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -20,9 +20,9 @@ function About(){
                     <img className="img-thumbnail about-me-image" src="../../images/Self-photo.jpg" alt="Kaeneth"/>
                   </div>
                   <div className="col-md-9">
-                    <p className="card-text">Hello, my name is Kaeneth Dayao. I graduated from the University of California, Irvine in 2012. After college, I worked at several telecommunication startup companies in leadership roles. I have over three years of supervisor and managerial experience. My experience has taught me to be a quick thinker, a self-starter, a problem-solver, and a team player. </p>
-                    <p>During my time working in startups, I started to become interested with app and web development. I decided to start my journey and began to learn different programming languages to become a developer. As I continue to learn, I am becoming more excited with this field. I am always eager to learn new skills in order to reach my career goal. My goal is to be a full stack web developer and create useful websites and mobile applications that can help people in their everyday lives.</p>
-                    <p>Lastly, I started a youtube channel that consist of short videos to help others with their coding journey.</p>
+                    <p className="card-text">Hello, my name is Kaeneth Dayao. I graduated from the University of California, Irvine. After college, I worked at several telecommunication startup companies in leadership roles. I have over three years of supervisor and managerial experience. My experience has taught me to be a quick thinker, a self-starter, a problem-solver, and a team player.</p>
+                    <p className="card-text">During my time working in startups, I worked closely with web developers and project managers. This is when I started to become interested with app and web development. I decided to start my journey and began to learn different programming languages to become a developer. As I continue to learn, I become more excited with this field. I am always eager to learn new skills in order to reach my career goal and create useful websites and applications that help people with their everyday lives.</p>
+                    <p className="card-text">I know this journey to be a developer can be difficult. I started a YouTube channel that consist of short coding videos to help others with their coding journey. The link to that channel can be found on the contact page. </p>
                   </div>
                 </div>
             </div>
