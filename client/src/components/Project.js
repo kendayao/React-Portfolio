028b5d043461822dbfc819eb1167ef58df2d53b7
Kaeneth Dayao <kendayao@gmail.com>
2020-08-13 18:47:17 -0700

    added video text

--- client/src/components/Project.js
+++ client/src/components/Project.js
@@ -10,7 +10,7 @@ function Project(props){
               <img className="img-thumbnail portfolio-image" src={item.img} alt="assemble-app" />
               <a href={item.appLink} target="_blank" class="links"><h2 className="card-text">{item.appTitle}</h2></a>
               <a href={item.appRepo} target="_blank" class="links"><h6 className="card-text-details">Project Details</h6></a>
-              <a href={item.appDemo} target="_blank" class="links"><h6 className="card-text-demo">Demo</h6></a>
+              <a href={item.appDemo} target="_blank" class="links"><h6 className="card-text-demo">Video Demo</h6></a>
               </div>
               <div class="middle">
                 <div class="text">
