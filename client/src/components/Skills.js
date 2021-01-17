f00e621e723d89ef3daeb6e0a887587bd295c0e6
Kaeneth Dayao <kendayao@gmail.com>
2021-01-16 22:31:57 -0800

    added new skills

--- client/src/components/Skills.js
+++ client/src/components/Skills.js
@@ -12,14 +12,14 @@ return (
         <i className="fab fa-html5 fa-3x skills-icon" title="HTML"></i>
         <i className="fab fa-node fa-3x skills-icon" title="Node.js"></i>
         <i className="fab fa-css3-alt fa-3x skills-icon" title="CSS"></i>
-        <img className="img1 skills-icon" title="MySQL" src="../images/mysql.png" alt="mysql" />
+        <img className="img1 skills-icon" title="MySQL" src="https://download.logo.wine/logo/MySQL/MySQL-Logo.wine.png" alt="mysql" />
         <i className="fab fa-bootstrap fa-3x skills-icon" title="Bootstrap"></i>
         <i className="fab fa-github fa-3x skills-icon" title="Github"></i>
         <i className="fab fa-react fa-3x skills-icon" title="REACT" ></i>
-        <img className="img1 skills-icon" title="jQuery" src="../images/jquery.png" alt="jquery"/>
+        <img className="img1 skills-icon" title="jQuery" src="https://openjsf.org/wp-content/uploads/sites/84/2019/10/jquery-logo-vertical_large_square.png" alt="jquery"/>
         <img className="img1 skills-icon" title="MERN" src="../images/mern.png" alt="Mern"/>
-        <img className="img1 skills-icon" title="MongoDB" src="../images/mongdb.png" alt="mongodb"/>
-        <img className="img1 skills-icon" title="Sequelize" src="../images/sequelize.png" alt="sequelize"/>
+        <img className="img1 skills-icon" title="MongoDB" src="https://infinapps.com/wp-content/uploads/2018/10/mongodb-logo.png" alt="mongodb"/>
+        <img className="img1 skills-icon" title="Sequelize" src="https://cdn.iconscout.com/icon/free/png-256/sequelize-3-1175091.png" alt="sequelize"/>
         <i className="fab fa-js fa-3x skills-icon" title="JavaScript" ></i>
         <i className="fab fa-python fa-3x skills-icon" title="Python"></i>
         <i className="fab fa-npm fa-3x skills-icon" title="npm"></i>
@@ -27,6 +27,7 @@ return (
         <img className="img1 skills-icon-redux" title="Redux" src="https://img.stackshare.io/service/7374/react-redux.png" alt="redux"/>
         <i class="fab fa-php fa-3x skills-icon" title="Php"></i>
         <i class="fab fa-wordpress fa-3x skills-icon" title="Wordpress"></i>
+        <img className="img1 skills-icon-redux" title="GraphQL" src="https://davidwalsh.name/demo/graphql-intro/graphql.png" alt="graphql"/>
       </div>
     </div>
   </div>
